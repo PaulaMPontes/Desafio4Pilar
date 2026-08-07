bc5821ba32daf8be259db92772ca13135d9fa40b
Bruno <brunowacherwskii@gmail.com>
2026-08-07 11:57:57 -0300

    MISSAO-1: abstracao

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         SuperHeroi h = new SuperHeroi();
         h.nome = "Lisiane Reips";
-        h.sexo = Sexo.MULHER;
+        h.sexo = 1;
         h.altura = 1.75;
         h.identidadeSecreta = true;
         h.habilidades = new String[] { "super força", "visão de raio-x", "voar",
@@ -13,21 +13,11 @@ public class Main {
         h.altura = -300;
         h.nome = "";
         h.mostrar();
-
-        Personagem h = new Personagem() {
-            @Override
-            public String apresentar() {
-                return nome + " - " + sexo.getDescricao() + " - " + altura;
-            }
-        };
-
-        // Preenchendo os dados do Passo 1
-        h.nome = "Lisiane Reips";
-        h.sexo = Sexo.MULHER;
-        h.altura = 1.75;
-
-        // Mostra o resultado na tela
-        System.out.println(h.apresentar());
+        // a
+        // Personagem g = new Personagem();
+        // p.nome = "bruno";
+        // p.sexo = "HOMEM";
+        // p.altura = 1,89;
 
     }
 }
--- src/SuperHeroi.java
+++ src/SuperHeroi.java
@@ -1,7 +1,8 @@
 // NÃO ALTERE ANTES DE LER OS PASSOS ------
 public class SuperHeroi {
+
     public String nome;
-    public int sexo;                // 1=Mulher, 2=Homem, 3=Outro, 4=Prefiro não dizer
+    public int sexo; // 1=Mulher, 2=Homem, 3=Outro, 4=Prefiro não dizer
     public double altura;
     public boolean identidadeSecreta;
     public String[] habilidades;
